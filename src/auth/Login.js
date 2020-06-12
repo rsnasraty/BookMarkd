a70274ae7c3133cda49c535f2c82ea1b8f81b6f1
Roxanne Nasraty <rsnasraty@gmail.com>
2020-06-12 15:35:01 -0500

    Added login styling

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { Jumbotron, Container, Row, Col, Button, InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 
 const Login = (props) => {
   return (
     <div>
-      <InputGroup>
+    <Container>
+    <Row>
+    <Col>
+    <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">BookMark'd</h1>
+          <p className="lead">This is a placefiller for the app logo</p>
+        </Container>
+      </Jumbotron>
+      <InputGroup size="lg">
         <InputGroupAddon addonType="prepend">
           <InputGroupText>Username</InputGroupText>
         </InputGroupAddon>
         <Input placeholder="username" />
       </InputGroup>
-      <InputGroup>
+      <InputGroup size="lg">
       <InputGroupAddon addonType="prepend">
           <InputGroupText>Password</InputGroupText>
         </InputGroupAddon>
         <Input placeholder="password" />
       </InputGroup>
-      
-      
+      <br />
+      <Button color="secondary" size="mg">Submit</Button>
+      </Col>
+      </Row>
+      </Container>
       
     </div>
   );

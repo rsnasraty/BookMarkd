2a1ddc1da81cc29ebd4585692a0497787a3b3bc0
Roxanne Nasraty <rsnasraty@gmail.com>
2020-06-23 13:23:07 -0500

    Testing Edit

--- src/modules/UserManager.js
+++ src/modules/UserManager.js
@@ -38,6 +38,14 @@ export default {
       `${remoteURL}/readingMaterials?userId=${userId}&statusId=Completed`
     ).then(result => result.json());
   },
-  
+  update(editedCR) {
+    return fetch(`${remoteURL}/readingMaterials/${editedCompletedRead.id}&statusId=Completed`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(editedCR)
+    }).then(data => data.json());
+  }
   
 };
